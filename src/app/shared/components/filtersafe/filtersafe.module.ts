b94e76279bb46407d9e8ab79a9d468a2600890d7
Luis Enrique Ticona Goyzueta <goyzueta.luis@gmail.com>
2021-09-27 21:34:28 -0500

    mensaje aqui

--- src/app/shared/components/filtersafe/filtersafe.module.ts
+++ src/app/shared/components/filtersafe/filtersafe.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FiltersafeComponent } from './filtersafe.component';
 import { MaterialModule } from '../../material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   declarations: [ FiltersafeComponent ],
@@ -12,8 +12,10 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     MaterialModule,
     ReactiveFormsModule,
     FormsModule,
+    NgbModule,
   ],
   exports: [ FiltersafeComponent ],
+  bootstrap: [FiltersafeComponent]
 
 })
 export class FiltersafeModule { }
