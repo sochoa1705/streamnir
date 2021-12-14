ec5c4fdb9022e0744e9bcb0b0de7dd370000569d
Omar Villavicencio <omarvillavicencio.bg@expertiatravel.com>
2021-12-14 11:20:58 -0500

    Test/srevices

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,14 @@
-
+const api = 'https://gds.webfarefinder.com/ServiciosTarjetaAsistencia/api/'
 export const environment = {
   production: true,
-  urlBase: 'http://10.75.131.17:10508/api/productoAsistencia/',
+  endpoint: api,
+  urlBase: api,                         // PRODUCCIÓN
+  // urlBase: 'http://10.75.131.17:10508/api/productoAsistencia/',  // DESARROLLO
   serverUrlApi: 'http://localhost:8080/api/',
   urlLogin: 'http://localhost:64238/login',
   nameAppAC: 'Intranet',
   identifierAC: 'NMViajes - Assist Card',
-  codeEnvironmentAC: 'DESA/NMO/NMO',
+  codeEnvironmentAC: 'PROD/NMO/NMO',
   undidadNegocioAC: 1,
   dkAgenciaAC: '339',
   subcodigoAgenciaAC: '1',
