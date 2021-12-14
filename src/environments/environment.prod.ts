d277dc77c322b85fd7aa8f5f96a74425c662f2b3
Omar Villavicencio <omarvillavicencio.bg@expertiatravel.com>
2021-12-14 10:27:16 -0500

    Sprint3/fix-services

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,7 @@
-const api = 'https://gds.webfarefinder.com/ServiciosTarjetaAsistencia/api/'
 
 export const environment = {
   production: true,
-  endpoint: api,
-  //urlBase: api + 'tarjetaAsistencia/',                         // PRODUCCIÓN
-  urlBase: 'http://10.75.131.17:10508/api/productoAsistencia/',  // DESARROLLO
+  urlBase: 'http://10.75.131.17:10508/api/productoAsistencia/',
   serverUrlApi: 'http://localhost:8080/api/',
   urlLogin: 'http://localhost:64238/login',
   nameAppAC: 'Intranet',
