cf53ca7185f95745dbff66e364046e2050f98e2d
Omar Villavicencio <omarvillavicencio.bg@expertiatravel.com>
2022-01-05 01:50:20 -0500

    tarjeta de credito

---
+++ src/app/Services/updatePay/update-pay.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { ENDPOINT_API } from 'src/app/shared/constant';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UpdatePayService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  updatePay(payload: any): Observable<any>{
+    let url_api = `${environment.urlBase}${ENDPOINT_API.UPDATE_PAY}`
+
+    return this.http.post<any>(url_api, payload, { observe: 'response' }).pipe(
+      map((observe: any) => observe['body']['Resultado'])
+    )
+
+  }
+
+}
