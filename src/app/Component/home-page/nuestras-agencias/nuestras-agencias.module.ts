b9ff542cd11b36bab60e4dae1ca9991e3ba7a7ff
Omar Villavicencio <omarvillavicencio.bg@expertiatravel.com>
2022-01-11 00:47:17 -0500

    Mapa

--- src/app/Component/home-page/nuestras-agencias/nuestras-agencias.module.ts
+++ src/app/Component/home-page/nuestras-agencias/nuestras-agencias.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NuestrasAgenciasComponent } from './nuestras-agencias.component';
 import { MaterialModule } from '../../../shared/material.module';
+import { GoogleMapsModule } from '@angular/google-maps'
 
 @NgModule({
   declarations: [ NuestrasAgenciasComponent ],
   imports: [
     CommonModule,
+    GoogleMapsModule,
     MaterialModule,
   ],
   exports: [ NuestrasAgenciasComponent ],
