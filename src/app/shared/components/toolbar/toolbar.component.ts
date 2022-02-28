99b0fe7d8c89aa35d56e9a78baa1953d27cf0512
Omar Villavicencio <omarvillavicencio.bg@expertiatravel.com>
2022-02-28 00:36:44 -0500

    add tag

--- src/app/shared/components/toolbar/toolbar.component.ts
+++ src/app/shared/components/toolbar/toolbar.component.ts
@@ -8,40 +8,40 @@ import { AccountService, UserStorage } from 'src/app/Services/account/account.se
   styleUrls: ['./toolbar.component.scss']
 })
 export class ToolbarComponent implements OnInit {
-  
+
   @Input() menu: any[];
 
   isLogged = false;
-
-  user:UserStorage;
+  user: UserStorage;
 
   constructor(
     public route: Router,
-    public accountService:AccountService
+    public accountService: AccountService
   ) { }
 
-  ngOnInit() { 
-
-    this.accountService.isLogged().subscribe(logged=>{
+  ngOnInit() {
+    this.accountService.isLogged().subscribe(logged => {
       this.isLogged = logged;
 
-      if(this.isLogged){
+      if (this.isLogged) {
         this.user = this.accountService.getUserStorage();
       }
     })
-
-
   }
 
   toHome() {
     this.route.navigateByUrl('/')
   }
-  to(e: any){
+  to(e: any) {
     window.location.href = e;
   }
 
   showOptionUser: Boolean = false;
   showOption() {
-    this.showOptionUser = this.showOptionUser ? false : true;
+    this.showOptionUser = !this.showOptionUser
+  }
+  logout() {
+    this.accountService.signOut()
+    this.route.navigateByUrl("/")
   }
 }
