4e29b915a3b7a97c2f0c8128f205b76ef4994b77
Renzo <renzocand@gmail.com>
2022-03-16 00:11:24 -0500

    loading y validaciones

--- src/app/shared/validators/validators.service.ts
+++ src/app/shared/validators/validators.service.ts
@@ -7,7 +7,7 @@ import { AbstractControl, ValidationErrors } from '@angular/forms';
 export class ValidatorsService {
 
   public lettersPattern: string = "^[a-zA-Z ]+$";
-  public emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+  public emailPattern: string = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$";
   public alphanumericPattern: string = "^[a-zA-Z0-9 ]+$";
   public digitsPattern: string = "^[0-9]+$";
 
