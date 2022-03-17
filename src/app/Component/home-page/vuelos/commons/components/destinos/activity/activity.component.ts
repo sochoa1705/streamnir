b17741bf1dcb903576028d33c11516e1f121712f
César Vallejo <cesarvallejo09@gmail.com>
2022-03-17 12:02:03 -0500

    Se oculta las secciones de actividades y hoteles en caso no tenga data.

--- src/app/Component/home-page/vuelos/commons/components/destinos/activity/activity.component.ts
+++ src/app/Component/home-page/vuelos/commons/components/destinos/activity/activity.component.ts
@@ -70,6 +70,7 @@ export class ActivityComponent implements OnInit {
       'Caller.Company': "Agil",
       'Caller.Application': "Interagencias"
     }).subscribe((res: any) => {
+      debugger
       this.tickets = JSON.parse(res).Result;
     });
   }
