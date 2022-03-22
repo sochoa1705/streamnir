5afba48cc64371cebfc458767ba732319ca30f78
César Vallejo <cesarvallejo09@gmail.com>
2022-03-21 23:10:15 -0500

    Independización de widgets (hoteles y actividades)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './Component/home-page/home/home.component';
-import { ActivityComponent } from './Component/home-page/vuelos/commons/components/destinos/activity/activity.component';
-import { HotelsComponent } from './Component/home-page/vuelos/commons/components/destinos/hotels/hotels.component';
 
 const routes: Routes = [
-  //{ path: '', redirectTo: 'home', pathMatch: 'full' },
-  {
-    path: '',
-    loadChildren: () => import('./Component/home-page/home-page.module').then(m => m.HomePageModule)
-  },
-  { path: 'hoteles/ciudad/:city/sitio/:site/vuelos/:isflight', component: HotelsComponent },
-  { path: 'actividades/ciudad/:city/sitio/:site/vuelos/:isflight', component: ActivityComponent },
-  { path: '**', component: HomeComponent }
+  { path: '', loadChildren: () => import('./Component/home-page/home-page.module').then(m => m.HomePageModule) },
+  { path: '**', redirectTo: '' }
 ]
 
 @NgModule({
