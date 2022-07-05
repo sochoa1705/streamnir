253ea8b5158e2c0870bdccd3f9f7a722954612f3
César Vallejo <cesarvallejo09@gmail.com>
2022-07-05 16:17:10 -0500

    Componente de paquetes como 404 con header y footer

--- src/app/Component/home-page/paquetes/paquetes.module.ts
+++ src/app/Component/home-page/paquetes/paquetes.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaquetesComponent } from './paquetes.component';
 
-
-
 @NgModule({
   declarations: [
     PaquetesComponent
