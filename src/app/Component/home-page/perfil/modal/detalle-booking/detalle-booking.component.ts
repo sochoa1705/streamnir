6e28268adf6eb2cf8b5cb56f7ef0f851a80267be
Josep <josepfs_1995@hotmail.com>
2022-08-26 10:47:58 -0500

    fix numero_vuelos

--- src/app/Component/home-page/perfil/modal/detalle-booking/detalle-booking.component.ts
+++ src/app/Component/home-page/perfil/modal/detalle-booking/detalle-booking.component.ts
@@ -5,23 +5,30 @@ import { DetalleBookingService } from './detalle-booking.service';
 @Component({
   selector: 'app-detalle-booking',
   templateUrl: './detalle-booking.component.html',
-  styleUrls: ['./detalle-booking.component.scss']
+  styleUrls: ['./detalle-booking.component.scss'],
 })
 export class DetalleBookingComponent implements OnInit {
   @Input() cot_Id: string = '';
   @Input() reserva_Id: string = '';
   @Input() tipo_Vuelo: string = '';
   data: any;
-  constructor(private detalleBookingService: DetalleBookingService, 
-    public ngbActiveModal: NgbActiveModal) {
-  }
+  constructor(
+    private detalleBookingService: DetalleBookingService,
+    public ngbActiveModal: NgbActiveModal
+  ) {}
 
   ngOnInit(): void {
     this.getDetalleBooking();
   }
-  private getDetalleBooking(){
-    this.detalleBookingService.getBookingDetail(this.cot_Id, this.reserva_Id, this.tipo_Vuelo).subscribe((data) => {
-      this.data = data;
-    });
+  getAirlineName(text: string): string {
+    return text.split('|')[1];
+  }
+  private getDetalleBooking() {
+    this.detalleBookingService
+      .getBookingDetail(this.cot_Id, this.reserva_Id, this.tipo_Vuelo)
+      .subscribe((data) => {
+        this.data = data;
+        console.log(this.data);
+      });
   }
 }
