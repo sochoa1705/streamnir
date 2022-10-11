bbe34e0f2e05e044c94882ca47f484635b9fbe02
César Vallejo <cesarvallejo09@gmail.com>
2022-10-11 12:12:05 -0500

    Ocultar controles para Multidestino

--- src/app/shared/components/calendar/calendar.component.ts
+++ src/app/shared/components/calendar/calendar.component.ts
@@ -18,7 +18,7 @@ export class CalendarComponent implements AfterViewInit {
   @Input() fromDate: NgbDate | null;
 
   @Input() typeValue: number = EnumFlightType.ida_vuelta;
-  @Input() placeHolder: string = 'Salidax';
+  @Input() placeHolder: string = 'Salida';
 
   @Input() requiredToDate: boolean;
   @Input() requiredFromDate: boolean;
