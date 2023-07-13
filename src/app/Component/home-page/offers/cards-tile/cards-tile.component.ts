af0e564f318c6a5b0e56b0fdb68f206f9d2aeea8
Samuel Huamán <samuelhuaman.dev@gmail.com>
2023-07-13 16:18:18 -0500

    Mejoras en Ofertas

--- src/app/Component/home-page/offers/cards-tile/cards-tile.component.ts
+++ src/app/Component/home-page/offers/cards-tile/cards-tile.component.ts
@@ -1,22 +1,20 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
 	selector: 'app-cards-tile',
 	templateUrl: './cards-tile.component.html',
 	styleUrls: [ './cards-tile.component.scss' ]
 })
-export class CardsTileComponent implements OnInit {
+export class CardsTileComponent {
+	@Input() id: string;
 	@Input() list: any[];
 	@Output() clickCard = new EventEmitter<string>();
 
 	constructor() { }
 
-	ngOnInit(): void {
-	}
-
 	slide(direction: number) {
-		const cardWrapper = document.querySelector('.card-slider-inner')!;
-		const cardWidth = document.querySelector('.card-wrapper')!.clientWidth;
+		const cardWrapper = document.querySelector(`#${this.id}-sliderInner`)!;
+		const cardWidth = document.querySelector(`#${this.id}-wrapper`)!.clientWidth;
 		const slideAmount = (cardWidth + 10) * direction; // Assuming a 10px margin between cards
 
 		cardWrapper.scrollBy({ left: slideAmount, behavior: 'smooth' });
