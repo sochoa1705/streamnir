068078c497537a9b30da1524dd9899bce3d413ba
unknown <tornelgabriela@gmail.com>
2023-09-12 17:32:20 -0500

    ref-button-alt

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -7,20 +7,21 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 })
 
 export class ButtonComponent implements OnInit {
-    @Input() text: string='';
-	@Input() disabled = false;
-	@Input() color = 'primary'; 
-	@Input() icon?: string;
-	@Input() right?: boolean;
-	@Input() left?: boolean;
-    @Input() w100=false;
-    @Input() size='md';
+    @Input() text: string = '';
+    @Input() disabled = false;
+    @Input() color = 'primary';
+    @Input() icon?: string;
+    @Input() right?: boolean;
+    @Input() left?: boolean;
+    @Input() w100 = false;
+    @Input() size = 'md';
+    @Input() isMobile = false;
     @Output() onClick = new EventEmitter<MouseEvent>();
 
     constructor() { }
     ngOnInit() { }
 
-    clickButton(){
+    clickButton() {
         this.onClick.emit()
     }
 }
