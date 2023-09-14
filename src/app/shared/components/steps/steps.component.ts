045ef73d967b8cb73991114e2832b7e3f59403f9
unknown <tornelgabriela@gmail.com>
2023-09-14 10:00:26 -0500

    ref-steps-resposive

--- src/app/shared/components/steps/steps.component.ts
+++ src/app/shared/components/steps/steps.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 import { Step } from 'src/app/api/api-checkout/models/rq-checkout-up-sell';
 import { CheckoutService } from 'src/app/api/api-checkout/services/checkout.service';
@@ -13,8 +13,10 @@ import { dataSteps } from '../../constant-init';
 export class StepsComponent implements OnInit {
     dataSteps:Step[]=dataSteps;
     isDisabled=false;
+
     @Output() clickedStep = new EventEmitter();
     constructor(private _checkoutService:CheckoutService, private _router:Router) {
+        this.updateStepName();
         this._checkoutService.changeStep.subscribe({
 			next: () => {
 				this.dataSteps=dataSteps;
@@ -33,5 +35,22 @@ export class StepsComponent implements OnInit {
     clickStep(index:number){
         if(!this.isDisabled)
         this.clickedStep.emit(index);
+    }
+    @HostListener('window:resize', ['$event'])
+    onResize(event: any) {
+      this.updateStepName();
+    }
+  
+    private updateStepName() {
+      // Check if the window width is less than 575px
+      if (window.innerWidth < 575) {
+        // Update the name of the third step
+        const stepIndex = 2; // Index of the step you want to update
+        this.dataSteps[stepIndex].name = 'Pagos';
+      } else {
+        // If the window width is not less than 575px, reset the name
+        const stepIndex = 2; // Index of the step you want to reset
+        this.dataSteps[stepIndex].name = 'Revisar y pagar';
+      }
     }
 }
