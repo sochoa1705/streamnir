672a1258efb5afd2295e03002e52f9d1d7eef9db
alle0n <dev.iparraguirre@gmail.com>
2023-11-10 10:15:32 -0500

    feat: formulario de eventos

---
+++ src/app/Models/eventos/post.events.interface.ts
@@ -0,0 +1,14 @@
+export interface SendEventsRequest {}
+
+interface Passenger {
+  docType: string;
+  docNumber: string;
+  names: string;
+  lastName: string;
+  mLastName: string;
+  email: string;
+  birthDate: string;
+  phone: string;
+}
+
+export interface SendEventsResponse {}
--- src/app/Services/presenter/data-page-presenter.models.ts
+++ src/app/Services/presenter/data-page-presenter.models.ts
@@ -18,5 +18,6 @@ export enum EGalleryCode {
 	banners_corporativos = 'BANNERS_CORPORATIVO',
 	banner_principal = 'BANNER_PRINCIPAL',
 	banner_principal_mobile = 'BANNER_PRINCIPAL_MOBILE',
-	banner_tus_datos_cp = 'TUS_DATOS'
-} 
+	banner_tus_datos_cp = 'TUS_DATOS',
+	banner_events_cp = 'EVENTOS'
+}
