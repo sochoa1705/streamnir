395efb1d6eafb0046c8d648e6c13e7bc7721b881
Sebastian <sebastianalejan.ochoa303@comunidadunir.net>
2024-05-12 21:28:36 -0500

    init project

---
+++ act-ind-sebastian-ochoa/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import {StreamingRouter} from "./router/StreamingRouter";
+
+function App() {
+  return (
+     <>
+         <StreamingRouter></StreamingRouter>
+     </>
+  );
+}
+
+export default App;
